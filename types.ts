5bad511909f9596a508a7e22a2ada80de456c156
Nick McCleery <n.mccleery@live.co.uk>
2025-02-03 17:58:09 +0000

    File generation and just types for now.

---
+++ types.ts
@@ -0,0 +1,73 @@
+export type Point = {
+  x: number
+  y: number
+}
+
+export interface ViewBox {
+  width: number
+  height: number
+}
+
+export interface SVGFile {
+  viewBox: ViewBox
+  paths: string[]
+}
+
+export enum CommandType {
+  NotSet = 'NotSet',
+  MoveAbsolute = 'MoveAbsolute',
+  MoveRelative = 'MoveRelative',
+  LineAbsolute = 'LineAbsolute',
+  LineRelative = 'LineRelative',
+  HorizontalLineAbsolute = 'HorizontalLineAbsolute',
+  HorizontalLineRelative = 'HorizontalLineRelative',
+  VerticalLineAbsolute = 'VerticalLineAbsolute',
+  VerticalLineRelative = 'VerticalLineRelative',
+  QuadraticBezierAbsolute = 'QuadraticBezierAbsolute',
+  QuadraticBezierRelative = 'QuadraticBezierRelative',
+  QuadraticBezierSmoothAbsolute = 'QuadraticBezierSmoothAbsolute',
+  QuadraticBezierSmoothRelative = 'QuadraticBezierSmoothRelative',
+  CubicBezierAbsolute = 'CubicBezierAbsolute',
+  CubicBezierRelative = 'CubicBezierRelative',
+  CubicBezierSmoothAbsolute = 'CubicBezierSmoothAbsolute',
+  CubicBezierSmoothRelative = 'CubicBezierSmoothRelative',
+  EllipticalArcAbsolute = 'EllipticalArcAbsolute',
+  EllipticalArcRelative = 'EllipticalArcRelative',
+  StopAbsolute = 'StopAbsolute',
+  StopRelative = 'StopRelative'
+}
+
+// Map from SVG command characters to descriptive names.
+export const SVGCommandMap: Record<string, CommandType> = {
+  M: CommandType.MoveAbsolute,
+  m: CommandType.MoveRelative,
+  L: CommandType.LineAbsolute,
+  l: CommandType.LineRelative,
+  H: CommandType.HorizontalLineAbsolute,
+  h: CommandType.HorizontalLineRelative,
+  V: CommandType.VerticalLineAbsolute,
+  v: CommandType.VerticalLineRelative,
+  Q: CommandType.QuadraticBezierAbsolute,
+  q: CommandType.QuadraticBezierRelative,
+  T: CommandType.QuadraticBezierSmoothAbsolute,
+  t: CommandType.QuadraticBezierSmoothRelative,
+  C: CommandType.CubicBezierAbsolute,
+  c: CommandType.CubicBezierRelative,
+  S: CommandType.CubicBezierSmoothAbsolute,
+  s: CommandType.CubicBezierSmoothRelative,
+  A: CommandType.EllipticalArcAbsolute,
+  a: CommandType.EllipticalArcRelative,
+  Z: CommandType.StopAbsolute,
+  z: CommandType.StopRelative
+}
+
+export const NumericVals: Array<string> = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']
+
+export interface PathState {
+  command: CommandType
+  values: number[]
+  valueBuffer: string
+  currentPoint: Point
+  isPathOpen: boolean
+  isValuePushed: boolean
+}

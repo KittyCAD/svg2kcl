473f58ea6588631ab3ebfdd71163c88a6d3c6fab
Nick McCleery <n.mccleery@live.co.uk>
2025-02-12 12:56:38 +0000

    Add mixed transforms.

--- tests/transforms.test.ts
+++ tests/transforms.test.ts
@@ -81,31 +81,17 @@ describe('SVG to KCL Conversion', () => {
     expect(actualKCL.trim()).toBe(expectedKCL.trim())
   })
 
-  // it('should correctly convert mixed_transforms.svg to KCL', async () => {
-  //   const inputPath = path.join(dataDir, 'mixed_transforms.svg')
-  //   const outputPath = path.join(dataDir, 'output.kcl')
-  //   const expectedKCLPath = path.join(dataDir, 'mixed_transforms.kcl')
+  it('should correctly convert mixed_transforms.svg to KCL', async () => {
+    const inputPath = path.join(dataDir, 'mixed_transforms.svg')
+    const outputPath = path.join(dataDir, 'output.kcl')
+    const expectedKCLPath = path.join(dataDir, 'mixed_transforms.kcl')
 
-  //   // Run the conversion.
-  //   await convertSVGtoKCL(inputPath, outputPath, options)
-  //   const actualKCL = await fsPromises.readFile(outputPath, 'utf8')
-  //   const expectedKCL = await fsPromises.readFile(expectedKCLPath, 'utf8')
+    // Run the conversion.
+    await convertSVGtoKCL(inputPath, outputPath, options)
+    const actualKCL = await fsPromises.readFile(outputPath, 'utf8')
+    const expectedKCL = await fsPromises.readFile(expectedKCLPath, 'utf8')
 
-  //   // Compare output with expected result.
-  //   expect(actualKCL.trim()).toBe(expectedKCL.trim())
-  // })
-
-  // it('should correctly convert project_payload.svg to KCL', async () => {
-  //   const inputPath = path.join(dataDir, 'project_payload.svg')
-  //   const outputPath = path.join(dataDir, 'output.kcl')
-  //   const expectedKCLPath = path.join(dataDir, 'project_payload.kcl')
-
-  //   // Run the conversion.
-  //   await convertSVGtoKCL(inputPath, outputPath, options)
-  //   const actualKCL = await fsPromises.readFile(outputPath, 'utf8')
-  //   const expectedKCL = await fsPromises.readFile(expectedKCLPath, 'utf8')
-
-  //   // Compare output with expected result.
-  //   expect(actualKCL.trim()).toBe(expectedKCL.trim())
-  // })
+    // Compare output with expected result.
+    expect(actualKCL.trim()).toBe(expectedKCL.trim())
+  })
 })

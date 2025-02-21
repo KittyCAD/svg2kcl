571f1848f3fe3b2969332c6a457729a812a072c3
Nick McCleery <n.mccleery@live.co.uk>
2025-02-21 16:22:54 +0000

    Nasty solve tol issue.

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
 // Solve tolerance for 'coincident' point distance.
-export const EPSILON_INTERSECT = 1e-3 // 1 micron for mm scale.
+export const EPSILON_INTERSECT = 1e-1 // 1 micron for mm scale.
 
 // Number of samples to take when sampling Bézier curves for intersection detection.
-export const N_CURVE_SAMPLES = 500
+export const N_CURVE_SAMPLES = 5000
 
 // Number of samples to take when sampling Bézier curve fragments for boundary detection.
 export const N_CURVE_SAMPLES_BOUNDARY = 500

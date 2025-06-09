30f7ff50d6d5a8adf0034e913060142166d2a124
Nick McCleery <n.mccleery@live.co.uk>
2025-06-09 18:08:33 +0100

    Config changes. MAY IMPACT DEBUG.

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,17 @@
 import type { Config } from 'jest'
 
-export default async (): Promise<Config> => {
-  return {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    verbose: false
+const config: Config = {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true
+    }
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1' // avoid .js import issues
   }
 }
+
+export default config
